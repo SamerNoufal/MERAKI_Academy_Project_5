ea8991f619854dd02209f1f1d91c8eb9616c893b
SamerNoufal <samer.noufal13@gmail.com>
2022-02-12 20:14:49 +0200

    Geo

--- frontend/src/component/Geolocate/geoLocate.js
+++ frontend/src/component/Geolocate/geoLocate.js
@@ -4,6 +4,7 @@ import "./geoLocate.css";
 import jwt_decode from "jwt-decode";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
 const GeoLocate = () => {
   const [lat, setLat] = useState("");
@@ -13,14 +14,20 @@ const GeoLocate = () => {
 
     navigator.geolocation.getCurrentPosition((position) => {
       console.log(position);
-      location = position;
+      //   location = position;
       setLat(position.coords.latitude);
       setLong(position.coords.longitude);
+      console.log(lat);
     });
   } else {
     console.log("geolocation not available");
   }
-  return <p>{lat}</p>;
+  return (
+    <div>
+      <p> Lat: {lat}</p>
+      <p> Long: {long}</p>
+    </div>
+  );
 };
 
 export default GeoLocate;
